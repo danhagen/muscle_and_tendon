c0fa3bf97ab6fc2d4daefba318e8589910424716
danhagen <dhagen@usc.edu>
2020-08-18 13:22:54 -0700

    small ref traj change

--- IB_1DOF_2DOA_simulation/pendulum_eqns/reference_trajectories/_01.py
+++ IB_1DOF_2DOA_simulation/pendulum_eqns/reference_trajectories/_01.py
@@ -11,7 +11,7 @@ Freq = 2*np.pi
 
 ### Reference Trajectory ###
 
-r = lambda t: Amp*np.cos(Freq*t) + Base
+r = lambda t:  Amp*np.cos(Freq*t) + Base
 dr = lambda t: -Amp*Freq*np.sin(Freq*t)
 d2r = lambda t: -Amp*Freq**2*np.cos(Freq*t)
 d3r = lambda t: Amp*Freq**3*np.sin(Freq*t)
